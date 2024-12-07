6755f7f83ae43980b161de00fabe1fdde3776461
rozalinutza22 <rozalinutza@protonmail.com>
2024-12-07 22:30:34 +0200

    Added error handling for folder path & json file path

--- main_script.py
+++ main_script.py
@@ -1,8 +1,18 @@
 import os
 import sys
 
+class MyException(Exception):
+    pass
+
 def create_structure(directory_path, json_path):
-    print("Hello")
+    if not os.path.isdir(directory_path):
+        raise FileNotFoundError("Directory does not exist!")
+
+    if not os.path.isfile(json_path):
+        raise FileNotFoundError("JSON File does not exist!")
+
+    if not json_path.endswith('.json'):
+        raise MyException(f"File {os.path.basename(json_path)} is not JSON")
 
 if __name__ == "__main__":
     try:
@@ -10,4 +20,4 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
 
-    create_structure(sys.argv[0], sys.argv[1])
+    create_structure(sys.argv[1], sys.argv[2])
