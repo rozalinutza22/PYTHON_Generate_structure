e4485bef4c8385e211ed995e1cac7a6a42bd08d1
rozalinutza22 <rozalinutza@protonmail.com>
2024-12-08 11:42:42 +0200

    Created empty_directory_module module which deletes all the files from the directory path

---
+++ modules/empty_directory_module.py
@@ -0,0 +1,20 @@
+import json
+import os
+from exception_module import MyException
+
+def empty_the_directory(directory_path):
+    for root, directories, files in os.walk(directory_path):
+        for fileName in files:
+            file_path = os.path.join(directory_path, fileName)
+
+            try:
+                
+                print(f"\nTrying to remove {fileName} ... ")
+                os.remove(file_path)
+                print(f"\nSuccessfully removed {fileName}")
+
+            except PermissionError:
+                raise MyException(f"\nInssuficient permissions to delete file {fileName}")
+            except Exception as e:
+                print(e)
+
