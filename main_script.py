f78aee45972967c851e1d08ba35412c091198017
rozalinutza22 <rozalinutza@protonmail.com>
2024-12-08 12:22:24 +0200

    Verified if the json content is read correctly

--- main_script.py
+++ main_script.py
@@ -14,6 +14,8 @@ def create_structure(directory_path, json_path):
 
     if not json_path.endswith('.json'):
         raise MyException(f"File {os.path.basename(json_path)} is not JSON")
+    
+    generate_structure_in_empty_folder(directory_path, json_path)
 
 if __name__ == "__main__":
     try:
--- modules/create_structure_in_empty_folder_module.py
+++ modules/create_structure_in_empty_folder_module.py
@@ -1,8 +1,13 @@
 import os
 import sys
-from modules.exception_module import MyException
 from modules.read_json_module import read_json_content
 
 def generate_structure_in_empty_folder(directory_path, json_path):
     structure = read_json_content(json_path)
-    print(type(structure))
+    
+    for key, value in structure.items():
+        if isinstance(value, dict):
+            print(f"{key} va fi folder")
+        else:
+            print(f"{key} va fi fisier")
+    
